7bccfe8c7191a145aa3a92a0df9172ff770519f1
RIDP87O <pier.paolo.annis@accenture.com>
2022-01-14 12:11:46 +0100

    add scan

--- src/android/DigitalAccessPlugin.java
+++ src/android/DigitalAccessPlugin.java
@@ -1,5 +1,9 @@
 package com.cordova.plugin.access;
 // The native Toast API
+import static java.security.AccessController.getContext;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.widget.Toast;
 // Cordova-required packages
 import org.apache.cordova.CallbackContext;
@@ -8,18 +12,41 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import zucchettiaxess.zreader.zblelib.Lib.AppsUtility.ErrorInfo;
+import zucchettiaxess.zreader.zblelib.Lib.HWIntegration.BLEScan;
+import zucchettiaxess.zreader.zblelib.Lib.HWIntegration.BleScanListener;
+
 public class DigitalAccessPlugin extends CordovaPlugin {
  
   private final String SHOW = "show";
+  private final String INIT = "init";
   private final String SCAN = "scan";
+
+  private ErrorInfo mainErrorInfo;
+  private BLEScan bleScan;
+  static Context mainContext;
+  private long timeoutScan = 10000;
 
   private static final String DURATION_LONG = "long";
   @Override
   public boolean execute(String action, JSONArray args,
     final CallbackContext callbackContext) {
 
-
+    PluginResult pluginResult;
       switch (action){
+        case INIT:
+
+          try {
+            JSONObject options = args.getJSONObject(0);
+            timeoutScan = options.getLong("timeoutScan");
+          } catch (JSONException e) {
+            callbackContext.error("Error encountered: " + e.getMessage());
+            return false;
+          }
+          pluginResult = new PluginResult(PluginResult.Status.OK);
+          callbackContext.sendPluginResult(pluginResult);
+          break;
         case SHOW:
             /*if (!action.equals("show")) {
             callbackContext.error("\"" + action + "\" is not a recognized action.");
@@ -41,21 +68,62 @@ public class DigitalAccessPlugin extends CordovaPlugin {
           // Display toast
           toast.show();
           // Send a positive result to the callbackContext
-          PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+          pluginResult = new PluginResult(PluginResult.Status.OK);
           callbackContext.sendPluginResult(pluginResult);
           break;
         case SCAN:
           //scan the badge
+          mainErrorInfo = new ErrorInfo();
+          mainErrorInfo.ClearAll();
+
+          mainContext = cordova.getContext();
+          bleScan = new BLEScan(mainContext, mainErrorInfo);
+
+          bleScan.startBLEScanning(mainErrorInfo, 110, bleScan,timeoutScan);
+
+          bleCallBack(bleScan, callbackContext);
 
 
-          
-          PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-          callbackContext.sendPluginResult(pluginResult);
           break;
 
+        default:
+          throw new IllegalStateException("Unexpected value: " + action);
       }
       // Verify that the user sent a 'show' action
      
       return true;
   }
+
+
+
+
+
+  private void bleCallBack(final BLEScan bleScan,final CallbackContext callbackContext) {
+    bleScan.setScanBleListener(new BleScanListener() {
+      @Override
+      public void onBleScanningTerminated() {
+        //no devices found?
+        if(BLEScan.getDevicesArray().size() == 0)
+        {
+
+          PluginResult pluginResult2 = new PluginResult(PluginResult.Status.OK, "No devices found" +timeoutScan);
+          callbackContext.sendPluginResult(pluginResult2);
+        }
+      }
+
+      @Override
+      public void onBleDeviceFound(final BluetoothDevice device) {
+        //add to textview
+        PluginResult pluginResult2 = new PluginResult(PluginResult.Status.OK, "Name: " + device.getName() +"\n" + "MAC:" + device.getAddress() +"\n\n" + timeoutScan);
+        callbackContext.sendPluginResult(pluginResult2);
+      }
+    });
+  
+
+
+
+
+  }
+
+
 }
