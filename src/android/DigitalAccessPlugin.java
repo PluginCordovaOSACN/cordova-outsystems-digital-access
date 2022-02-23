e3a9cfe3aab3f15b51adb148592d838c90e362cf
RIDP87O <pier.paolo.annis@accenture.com>
2022-02-23 18:27:00 +0100

    add try catch

--- src/android/DigitalAccessPlugin.java
+++ src/android/DigitalAccessPlugin.java
@@ -78,9 +78,8 @@ public class DigitalAccessPlugin extends CordovaPlugin {
     private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 1;
     private static final int MY_PERMISSION_ACCESS_BLUETOOTH = 2;
 
-    String [] permissionsLocation = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-    String [] permissionsBluetooth = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN };
-
+    String[] permissionsLocation = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+    String[] permissionsBluetooth = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN};
 
 
     @Override
@@ -88,7 +87,7 @@ public class DigitalAccessPlugin extends CordovaPlugin {
         super.onRequestPermissionResult(requestCode, permissions, grantResults);
         if (requestCode == MY_PERMISSION_ACCESS_COARSE_LOCATION) {
 
-            for(int i=0; i<grantResults.length; i++){
+            for (int i = 0; i < grantResults.length; i++) {
                 if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                     Toast.makeText(cordova.getActivity(),
                             "Location Permission is required for access!",
@@ -98,7 +97,7 @@ public class DigitalAccessPlugin extends CordovaPlugin {
 
         }
         if (requestCode == MY_PERMISSION_ACCESS_BLUETOOTH) {
-            for(int i=0; i<grantResults.length; i++) {
+            for (int i = 0; i < grantResults.length; i++) {
                 if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                     Toast.makeText(cordova.getActivity(),
                             "Bluetooth Permission is required for access! " + permissions[i],
@@ -110,10 +109,8 @@ public class DigitalAccessPlugin extends CordovaPlugin {
 
     public boolean hasPermisssionBluetooth() {
 
-        for(String p : permissionsBluetooth)
-        {
-            if(!PermissionHelper.hasPermission(this, p))
-            {
+        for (String p : permissionsBluetooth) {
+            if (!PermissionHelper.hasPermission(this, p)) {
                 return false;
             }
         }
@@ -121,10 +118,8 @@ public class DigitalAccessPlugin extends CordovaPlugin {
     }
 
     public boolean hasPermisssionLocation() {
-        for(String p : permissionsLocation)
-        {
-            if(!PermissionHelper.hasPermission(this, p))
-            {
+        for (String p : permissionsLocation) {
+            if (!PermissionHelper.hasPermission(this, p)) {
                 return false;
             }
         }
@@ -143,15 +138,15 @@ public class DigitalAccessPlugin extends CordovaPlugin {
                             public void onClick(DialogInterface dialog, int which) {
                                 bluetoothAdapter.enable();
 
-                     }
-                    })
-                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .create().show();
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
                 return false;
             }
             return true;
@@ -171,152 +166,185 @@ public class DigitalAccessPlugin extends CordovaPlugin {
 
         switch (action) {
             case INIT:
-                   if (args != null && args.length() > 0) {
-                    try {
-                        timeoutScan = args.getLong(0);
-                        if (args.length() > 1) {
-                            dbDistance = args.getInt(1);
+                try {
+
+                    if (args != null && args.length() > 0) {
+                        try {
+                            timeoutScan = args.getLong(0);
+                            if (args.length() > 1) {
+                                dbDistance = args.getInt(1);
+                            }
+                            if (args.length() > 2) {
+                                buildingDefault = args.getString(2);
+                            }
+                            if (args.length() > 3) {
+                                badgeCode = args.getString(3);
+                            }
+                        } catch (JSONException e) {
+                            result.setSuccess(false);
+                            result.setMessage(e.getMessage());
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
                         }
-                        if (args.length() > 2) {
-                            buildingDefault = args.getString(2);
-                        }
-                        if (args.length() > 3) {
-                            badgeCode = args.getString(3);
-                        }
-                    } catch (JSONException e) {
-                        result.setSuccess(false);
-                        result.setMessage(e.getMessage());
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
                     }
+
+                    init(INIT);
+
+
+                    pluginResult = getPluginResult(PluginResult.Status.OK);
+                    callbackContext.sendPluginResult(pluginResult);
+                } catch (Exception e) {
+                    if (result == null) {
+                        result = new Result();
+                        result.setMethod(INIT);
+                    }
+                    result.setSuccess(false);
+                    result.setMessage(e.getMessage());
+                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+
                 }
-
-                init(INIT);
-
-
-                pluginResult = getPluginResult(PluginResult.Status.OK);
-                callbackContext.sendPluginResult(pluginResult);
                 break;
 
             case SCAN:
+                try {
+
+                    boolean isUsingFakeDevice = false;
+                    if (args != null && args.length() > 0) {
+                        try {
+                            badgeCode = args.getString(0);
+                            if (args.length() > 1) {
+                                isUsingFakeDevice = args.getBoolean(1);
+
+                            }
+                        } catch (JSONException e) {
+                            result = new Result();
+                            result.setMethod(SCAN);
+                            result.setSuccess(false);
+                            result.setMessage(e.getMessage());
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+                        }
+                    }
+
+                    if (result == null || !badgeCode.equals(result.getBadgeCode())) {
+                        if (!init(SCAN)) {
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+                        }
+                        ;
+                    }
+                    result.setMethod(SCAN);
 
 
-                boolean isUsingFakeDevice = false;
-                if (args != null && args.length() > 0) {
-                    try {
-                        badgeCode = args.getString(0);
-                        if (args.length() > 1) {
-                            isUsingFakeDevice = args.getBoolean(1);
+                    if (!isUsingFakeDevice) {
+                        if (!cordova.getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                            cordova.getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(cordova.getContext(), "Error = ble NOT supported", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            result.setSuccess(false);
+
+                            result.setMessage("ble NOT supported");
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
 
                         }
-                    } catch (JSONException e) {
+
+                        if (checkPermission(true)) {
+
+
+                            bleScan = new BLEScan(mainContext, mainErrorInfo);
+                            bleScan.startBLEScanning(mainErrorInfo, dbDistance, bleScan, timeoutScan);
+
+                            bleCallBack(bleScan, callbackContext);
+
+                        } else {
+                            result.setSuccess(false);
+
+                            result.setMessage("checkPermission not allowed");
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+                        }
+                    } else {
+                        result.setSuccess(true);
+                        result.setMessage("FakeDevice found");
+                        result.setDeviceName(fakeDevice);
+
+                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.OK));
+                    }
+
+                } catch (Exception e) {
+                    if (result == null) {
                         result = new Result();
                         result.setMethod(SCAN);
-                        result.setSuccess(false);
-                        result.setMessage(e.getMessage());
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
                     }
+                    result.setSuccess(false);
+                    result.setMessage(e.getMessage());
+                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+
                 }
-
-                if (result == null || !badgeCode.equals(result.getBadgeCode())) {
-                    if (!init(SCAN)) {
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
-                    }
-                    ;
-                }
-                result.setMethod(SCAN);
-
-
-                if (!isUsingFakeDevice) {
-                    if (!cordova.getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-                        cordova.getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(cordova.getContext(), "Error = ble NOT supported", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                        result.setSuccess(false);
-
-                        result.setMessage("ble NOT supported");
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
-
-                    }
-
-                    if (checkPermission(true)) {
-
-
-                        bleScan = new BLEScan(mainContext, mainErrorInfo);
-                        bleScan.startBLEScanning(mainErrorInfo, dbDistance, bleScan, timeoutScan);
-
-                        bleCallBack(bleScan, callbackContext);
-
-                    } else {
-                        result.setSuccess(false);
-
-                        result.setMessage("checkPermission not allowed");
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
-                    }
-                } else {
-                    result.setSuccess(true);
-                    result.setMessage("FakeDevice found");
-                    result.setDeviceName(fakeDevice);
-
-                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.OK));
-                }
-
-
                 break;
             case SEND:
-                if (result == null) {
-                    if (!init(SEND)) {
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+                try {
+                    if (result == null) {
+                        if (!init(SEND)) {
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+                        }
+                        ;
                     }
-                    ;
-                }
-                result.setMethod(SEND);
+                    result.setMethod(SEND);
 
-                //simulate access with fake reader
-                if (fakeDevice.equals(result.getDeviceName())) {
-                    result.setSuccess(true);
-                    result.setMessage("Badge sended");
-
-                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.OK));
-                }
-
-                if (BLEScan.getBleScan() == null || BLEScan.getDevicesArray() == null || BLEScan.getDevicesArray().size() == 0) {
-                    result.setSuccess(false);
-                    result.setMessage("No reader found");
-                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
-                }
-
-                BLESend sender;
-                sender = new BLESend(cordova.getContext());
-                sendPD(true, "BLE Send in progress");
-
-                sender.sendBadgeToBle(BLEScan.getDevicesArray().get(0), mainErrorInfo, Dir_Type.DIR_IN);
-                sender.setBleListener(new BLESendListener() {
-                    @Override
-                    public void onBLEReaderDisconnected() {
-                        result.setSuccess(false);
-                        result.setMessage("Badge not send");
-                        bleScan.stopBLEScanning();
-                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
-
-                    }
-
-                    @Override
-                    public void onSendBadgeCompleted() {
+                    //simulate access with fake reader
+                    if (fakeDevice.equals(result.getDeviceName())) {
                         result.setSuccess(true);
                         result.setMessage("Badge sended");
-                        bleScan.stopBLEScanning();
+
                         callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.OK));
-
                     }
 
-                    @Override
-                    public void onNewActionInSend(f f) {
-
+                    if (BLEScan.getBleScan() == null || BLEScan.getDevicesArray() == null || BLEScan.getDevicesArray().size() == 0) {
+                        result.setSuccess(false);
+                        result.setMessage("No reader found");
+                        callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
                     }
-                });
+
+                    BLESend sender;
+                    sender = new BLESend(cordova.getContext());
+                    sendPD(true, "BLE Send in progress");
+
+                    sender.sendBadgeToBle(BLEScan.getDevicesArray().get(0), mainErrorInfo, Dir_Type.DIR_IN);
+                    sender.setBleListener(new BLESendListener() {
+                        @Override
+                        public void onBLEReaderDisconnected() {
+                            result.setSuccess(false);
+                            result.setMessage("Badge not send");
+                            bleScan.stopBLEScanning();
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+
+                        }
+
+                        @Override
+                        public void onSendBadgeCompleted() {
+                            result.setSuccess(true);
+                            result.setMessage("Badge sended");
+                            bleScan.stopBLEScanning();
+                            callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.OK));
+
+                        }
+
+                        @Override
+                        public void onNewActionInSend(f f) {
+
+                        }
+                    });
+
+                } catch (Exception e) {
+                    if (result == null) {
+                        result = new Result();
+                        result.setMethod(SEND);
+                    }
+                    result.setSuccess(false);
+                    result.setMessage(e.getMessage());
+                    callbackContext.sendPluginResult(getPluginResult(PluginResult.Status.ERROR));
+
+                }
                 break;
 
             case STOP:
@@ -367,24 +395,25 @@ public class DigitalAccessPlugin extends CordovaPlugin {
             return false;
         }
 
-        if(!isEnableBluetooth()){
+        if (!isEnableBluetooth()) {
             result.setSuccess(false);
             result.setMessage("Bluetooth disable");
             return false;
 
-        };
+        }
+        ;
 
-        if(!hasPermisssionBluetooth()){
+        if (!hasPermisssionBluetooth()) {
             result.setSuccess(false);
             result.setMessage("Permission Bluetooth disable");
-            PermissionHelper.requestPermissions(this,MY_PERMISSION_ACCESS_BLUETOOTH, permissionsBluetooth);
+            PermissionHelper.requestPermissions(this, MY_PERMISSION_ACCESS_BLUETOOTH, permissionsBluetooth);
             return false;
 
         }
-        if(!hasPermisssionLocation()){
+        if (!hasPermisssionLocation()) {
             result.setSuccess(false);
             result.setMessage("Permission Location disable");
-            PermissionHelper.requestPermissions(this,MY_PERMISSION_ACCESS_COARSE_LOCATION, permissionsLocation);
+            PermissionHelper.requestPermissions(this, MY_PERMISSION_ACCESS_COARSE_LOCATION, permissionsLocation);
             return false;
 
         }
@@ -440,7 +469,6 @@ public class DigitalAccessPlugin extends CordovaPlugin {
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
         return new PluginResult(status, gson.toJson(result));
     }
-
 
 
     private boolean checkPermission(boolean withAsk) {
