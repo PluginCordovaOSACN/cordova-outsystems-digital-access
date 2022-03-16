8c9a86238b8dfa684679f097627778c0e1a79c09
RIDP87O <pier.paolo.annis@accenture.com>
2022-03-16 17:33:18 +0100

    added sdk

---
+++ sdk/ios/ZAxessCommonObjects.xcframework/ios-arm64_x86_64-simulator/ZAxessCommonObjects.framework/Headers/ZAxessCommonObjects.h
@@ -0,0 +1,19 @@
+//
+//  ZAxessCommonObjects.h
+//  ZAxessCommonObjects
+//
+//  Created by Andrea Mancini on 16/08/17.
+//  Copyright © 2017 Zucchetti Axess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZAxessCommonObjects.
+FOUNDATION_EXPORT double ZAxessCommonObjectsVersionNumber;
+
+//! Project version string for ZAxessCommonObjects.
+FOUNDATION_EXPORT const unsigned char ZAxessCommonObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZAxessCommonObjects/PublicHeader.h>
+
+
